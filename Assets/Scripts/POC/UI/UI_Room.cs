2f807ce1469c8b67a693780291a55f8c7b0f194b
Chongcharen <chongcharen@hotmail.com>
2020-07-30 23:26:04 +0700

    no message

--- Assets/Scripts/POC/UI/UI_Room.cs
+++ Assets/Scripts/POC/UI/UI_Room.cs
@@ -62,6 +62,7 @@ public class UI_Room : MonoBehaviourPunCallbacks
             Debug.Log("Index "+index);
             Debug.Log("player value "+player.Value);
             Debug.Log("playersData data "+playersData);
+            Debug.Log("playersData data count"+playersData.Length);
             playersData[index].SetData(player.Value,playerColor[index]);
             playersData[index].gameObject.SetActive(true);
             PlayerIndexProfileData playerIndexProfileData;

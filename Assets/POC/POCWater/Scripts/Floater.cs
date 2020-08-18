8908362681c6d575331bc20ccf05a55b379f2c12
nattapong_prachan <chongcharen@hotmail.com>
2020-08-18 19:33:57 +0700

    no message

--- Assets/POC/POCWater/Scripts/Floater.cs
+++ Assets/POC/POCWater/Scripts/Floater.cs
@@ -11,7 +11,7 @@ public class Floater : MonoBehaviour
     void FixedUpdate(){
         if(transform.position.y < 0){
             float displacementMultiplier = Mathf.Clamp01(-transform.position.y / depthBeforeSubmerged)*displacementAmount;
-          //  rigidbody.AddForce(new Vector3(0))
+            rigidbody.AddForce(new Vector3(0,Mathf.Abs(Physics.gravity.y)*displacementMultiplier,0f),ForceMode.Acceleration);
         }
     }
 }

aa12720dabcacd440d47205091cf3cc703962363
Nattapong Prachan <chongcharen@hotmail.com>
2020-11-02 18:46:08 +0700

    no message

---
+++ Assets/Scripts/Detecter/CameraDetecter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UniRx;
+public class CameraDetecter : MonoBehaviour
+{
+    [SerializeField]CinemachineVirtualCamera virtualCamera;
+    private void Awake() {
+        //virtualCamera = GetComponent<CinemachineVirtualCamera>();   
+        BikeBoltSystem.OnCameraLookup.Subscribe(target =>{
+            virtualCamera.LookAt = target;
+            virtualCamera.Follow = target;
+        }).AddTo(this); 
+    }
+    private void Start() {
+         
+        Debug.Log("virtualcam "+virtualCamera);
+    }
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("ontriggerenter ");
+            virtualCamera.enabled = true;
+    }
+    private void OnTriggerExit(Collider other) {
+        Debug.Log("onenterExit");
+            virtualCamera.enabled = false;
+        //virtualCamera.des
+    }
+}
